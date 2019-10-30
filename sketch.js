5fa9667202ae13077430f961f2fbf9049d0ddc29
Masahide Maehara <maehrm@gmail.com>
2019-10-30 21:26:17 +0900

    sin,cos changed.

--- sketch.js
+++ sketch.js
@@ -7,20 +7,20 @@ function setup() {
     createCanvas(640, 480);
     noStroke();
 }
- 
+
 function draw() {
     background(255);
     push();                // 座標をcanvasの中心にするため一時的に保存
     translate(width / 2, height / 2);
     fill(127);
     ellipse(0, 0, r1 * 2, r1 * 2); // 円周（大）の描画
-    x = sin(radians(angle1)) * r1; // 円周上（大）のX座標の位置
-    y = cos(radians(angle1)) * r1; // 円周上（大）のY座標の位置
+    x = r1 * cos(radians(angle1)); // 円周上（大）のX座標の位置
+    y = r1 * sin(radians(angle1)); // 円周上（大）のY座標の位置
     push();                        // 座標を円周上（大）の(X,Y)座標にするため一時的に保存
     translate(x, y);            
     for (let i = 0; i < 8; i++) {
-        x = sin(radians(angle2 - (i * 45))) * r2; // 円周上のX座標の位置
-        y = cos(radians(angle2 - (i * 45))) * r2; // 円周上のY座標の位置
+        x = r2 * cos(radians(angle2 - (i * 45))); // 円周上のX座標の位置
+        y = r2 * sin(radians(angle2 - (i * 45))); // 円周上のY座標の位置
         fill(35 * i);
         ellipse(x, y, 10 - i, 10 - i); // アニメーションする円の描画
     }
