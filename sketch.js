1eb14e7e30ce1f14a4cc61def224daa4bb834e2b
Masahide Maehara <maehrm@gmail.com>
2019-10-30 21:16:00 +0900

    first commit

---
+++ sketch.js
@@ -0,0 +1,31 @@
+let angle1 = 0;                 // 大きな円上の角度
+let r1 = 100;                   // 円周(大)の半径
+let angle2 = 0;                 // 小さな円上の角度
+let r2 = 15;                    // 円周（小）の半径
+
+function setup() {
+    createCanvas(640, 480);
+    noStroke();
+}
+ 
+function draw() {
+    background(255);
+    push();                // 座標をcanvasの中心にするため一時的に保存
+    translate(width / 2, height / 2);
+    fill(127);
+    ellipse(0, 0, r1 * 2, r1 * 2); // 円周（大）の描画
+    x = sin(radians(angle1)) * r1; // 円周上（大）のX座標の位置
+    y = cos(radians(angle1)) * r1; // 円周上（大）のY座標の位置
+    push();                        // 座標を円周上（大）の(X,Y)座標にするため一時的に保存
+    translate(x, y);            
+    for (let i = 0; i < 8; i++) {
+        x = sin(radians(angle2 - (i * 45))) * r2; // 円周上のX座標の位置
+        y = cos(radians(angle2 - (i * 45))) * r2; // 円周上のY座標の位置
+        fill(35 * i);
+        ellipse(x, y, 10 - i, 10 - i); // アニメーションする円の描画
+    }
+    pop();
+    angle2 += 7;
+    pop();
+    angle1 += 1;
+}
